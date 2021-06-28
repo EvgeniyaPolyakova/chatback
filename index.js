663186a4e64dfe6cdf4dfbe3115004ba8b21947c
Evgeniya Polyakova <evgeniyapolyakova050000@yandex.ru>
2021-06-28 19:15:13 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,36 @@
+const e = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const server = http.createServer();
+
+const connections = [];
+
+server.listen(3001, () => {
+  console.log("server started");
+});
+
+const io = socketIo(server, {
+  cors: "*",
+});
+
+io.on("connection", (socket) => {
+  if (connections.length < 2) {
+    console.log("Успешно", socket.id);
+    connections.push(socket.id);
+    console.log(connections.length);
+
+    socket.on("message", (data) => {
+      io.emit('message', data)
+    });
+  } else {
+    console.log("Чат переполнен!", socket.id);
+    socket.disconnect();
+    
+  }
+
+  socket.on("disconnect", (socket) => {
+    connections.splice(connections.indexOf(socket.id), 1);
+    console.log("Отключились");
+  });
+});
